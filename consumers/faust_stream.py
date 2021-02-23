67c3121dd702d28d2f521e940bbe62cb83115962
Guillermo <guillermo.garcia.espuela@gmail.com>
2021-02-23 23:21:48 +0100

    Completed faust stream aplication to extract station dat

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -33,26 +33,35 @@ class TransformedStation(faust.Record):
 #   places it into a new topic with only the necessary information.
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
 # TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
+topic = app.topic("com.udacity.stations", value_type=Station)
 # TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
+out_topic = app.topic("com.udacity.output", partitions=1)
 # TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+table = app.Table(
+    "stations_table.v1",
+    default=str,
+    partitions=1,
+    changelog_topic=out_topic,
+)
 
+# Auxiliar function to detect line
+def detect_line(Station):
+    if(Station.red):
+        line = 'red'
+    elif(Station.blue):
+        line = 'red'
+    elif(Station.green):
+        line = 'green'
+    return line
+     
 
-#
-#
-# TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
-# "line" is the color of the station. So if the `Station` record has the field `red` set to true,
-# then you would set the `line` of the `TransformedStation` record to the string `"red"`
-#
-#
-
+@app.agent(topic)
+async def stationsEvent(stream):
+    async for st in stream:
+        line = detect_line(st)
+        tfs = TransformedStation(st.station_id, st.station_name,st.order,line)
+        table[st.station_id] = tfs
+        
 
 if __name__ == "__main__":
     app.main()
