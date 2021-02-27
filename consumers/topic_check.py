a0e4271feea0c421758ec736f285f69471d4d101
Guillermo <guillermo.garcia.espuela@gmail.com>
2021-02-27 13:30:41 +0100

    Uncomment functions in topic_check.py

--- consumers/topic_check.py
+++ consumers/topic_check.py
@@ -6,26 +6,25 @@ def topic_exists(topic):
     return topic in set(t.topic for t in iter(topic_metadata.topics.values()))
 
 
-# from confluent_kafka.admin import AdminClient
+from confluent_kafka.admin import AdminClient
 
+def topic_exists(topic):
+    """Checks if the given topic exists in Kafka"""
+    client = AdminClient({"bootstrap.servers": "PLAINTEXT://localhost:9092"})
+    topic_metadata = client.list_topics(timeout=5)
+    return topics in topic_metadata.topics
 
-# def topic_exists(topic):
-    # """Checks if the given topic exists in Kafka"""
-    # client = AdminClient({"bootstrap.servers": "PLAINTEXT://localhost:9092"})
-    # topic_metadata = client.list_topics(timeout=5)
-    # return topics in topic_metadata.topics
+def contains_substring(to_test, substr):
+    _before, match, _after = to_test.partition(substr)
+    return len(match) > 0
 
-# def contains_substring(to_test, substr):
-    # _before, match, _after = to_test.partition(substr)
-    # return len(match) > 0
-
-# def topic_pattern_match(pattern):
-    # """
-        # Takes a string `pattern`
-        # Returns `True` if one or more topic names contains substring `pattern`.
-        # Returns `False` if not.
-    # """
-    # topic_metadata = client.list_topics()
-    # topics = topic_metadata.topics
-    # filtered_topics = {key: value for key, value in topics.items() if contains_substring(key, pattern)}
-    # return len(filtered_topics) > 0
+def topic_pattern_match(pattern):
+    """
+        Takes a string `pattern`
+        Returns `True` if one or more topic names contains substring `pattern`.
+        Returns `False` if not.
+    """
+    topic_metadata = client.list_topics()
+    topics = topic_metadata.topics
+    filtered_topics = {key: value for key, value in topics.items() if contains_substring(key, pattern)}
+    return len(filtered_topics) > 0
